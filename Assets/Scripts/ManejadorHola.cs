8b3eb2e6f7004a99da16cd109d6bc16e81f1ba19
BorjaFdzAn <BorjaFdzAn@gmail.com>
2022-01-12 21:46:18 +0100

    Add panel de botones de host cliente y servidor

--- Assets/Scripts/ManejadorHola.cs
+++ Assets/Scripts/ManejadorHola.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class ManejadorHola : MonoBehaviour
+namespace HolaMundo
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class ManejadorHola : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            {
+                PonerBotones();
+            }
+            else
+            {
+                EtiquetasEstado();
+                EnviarNuevaPosicion();
+            }
+            GUILayout.EndArea();
+        }
+        private void PonerBotones()
+        {
+            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Servidor")) NetworkManager.Singleton.StartServer();
+        }
+
+        private void EtiquetasEstado()
+        {
+
+        }
+
+        private void EnviarNuevaPosicion()
+        {
+
+        }
     }
 }
